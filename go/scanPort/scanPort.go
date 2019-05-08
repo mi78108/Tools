ef6f0b821843668c5b8888395d0d060065d81a79
mi78108 <mi78108@live.com>
2019-05-08 11:05:26 +0800

    scanPort add get server information suppost

--- go/scanPort/scanPort.go
+++ go/scanPort/scanPort.go
@@ -1,71 +1,80 @@
 package main
 
 import (
-  "fmt"
-  "log"
-  "net"
-  "os"
-  "strconv"
-  "strings"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func connect(addr string, ch chan string) {
-  s, e := net.Dial("tcp", addr)
-  if e == nil {
-    ch <- fmt.Sprintf("The %c[1;32m%-21s%c[0m is   Connect able --%c[1;32m%s%c[0m", 0x1B,addr,0x1B,0x1B,"True",0x1B)
-    s.Close()
-  } else {
-    ch <- fmt.Sprintf("The %c[1;31m%-21s%c[0m is UnConnect able --%c[1;31m%s%c[0m", 0x1B,addr,0x1B,0x1B,"False",0x1B)
-  }
+	s, e := net.Dial("tcp", addr)
+	if e == nil {
+		buf := make([]byte, 1024)
+		s.SetReadDeadline(time.Now().Add(1 * time.Second))
+		n,_:=s.Read(buf)
+		for i, b := range buf[:n] {
+			if !strconv.IsPrint(int32(b)) {
+			buf[i]='\040'
+			}
+		}
+		ch <- fmt.Sprintf("The %c[1;32m%-21s%c[0m is   Connect able --%c[1;32m%s%c[0m	--> %c[1;35m%s%c[0m", 0x1B, addr, 0x1B, 0x1B, "True", 0x1B, 0x1B, strings.ReplaceAll(string(buf), "\n", " "), 0x1B)
+		s.Close()
+	} else {
+		ch <- fmt.Sprintf("The %c[1;31m%-21s%c[0m is UnConnect able --%c[1;31m%s%c[0m", 0x1B, addr, 0x1B, 0x1B, "False", 0x1B)
+	}
 }
 
 func main() {
-  var ports []uint16
-  ch := make(chan string)
-  all := false
+	var ports []uint16
+	ch := make(chan string)
+	all := false
 
-  if len(os.Args) < 3 || (len(os.Args) < 3 && strings.Contains(string(os.Args[1]), "-a")) {
-    log.Fatalln("usage: scanPort [-a] ip_addr port_range")
-  }
+	if len(os.Args) < 3 || (len(os.Args) < 3 && strings.Contains(string(os.Args[1]), "-a")) {
+		log.Fatalln("usage: scanPort [-a] ip_addr port_range")
+	}
 
-  port_v := string(os.Args[2])
-  host := string(os.Args[1])
+	port_v := string(os.Args[2])
+	host := string(os.Args[1])
 
-  if strings.Contains(string(os.Args[1]), "-a") {
-    port_v = string(os.Args[3])
-    host = string(os.Args[2])
-    all = true
-  }
+	if strings.Contains(string(os.Args[1]), "-a") {
+		port_v = string(os.Args[3])
+		host = string(os.Args[2])
+		all = true
+	}
 
-  if v,e:=strconv.Atoi(port_v);e==nil{
-    ports=append(ports,uint16(v))
-  }
+	if v, e := strconv.Atoi(port_v); e == nil {
+		ports = append(ports, uint16(v))
+	}
 
-  if n := strings.Index(port_v, "-"); n > 0 {
-    b, _ := strconv.Atoi(port_v[:n])
-    e, _ := strconv.Atoi(port_v[n+1:])
-    for i := b; i <= e; i++ {
-      ports = append(ports, uint16(i))
-    }
-  }
+	if n := strings.Index(port_v, "-"); n > 0 {
+		b, _ := strconv.Atoi(port_v[:n])
+		e, _ := strconv.Atoi(port_v[n+1:])
+		for i := b; i <= e; i++ {
+			ports = append(ports, uint16(i))
+		}
+	}
 
-  if strings.Contains(string(port_v), ",") {
-    ps := strings.Split(port_v, ",")
-    for _, p := range ps {
-      i, _ := strconv.Atoi(p)
-      ports = append(ports, uint16(i))
-    }
-  }
+	if strings.Contains(string(port_v), ",") {
+		ps := strings.Split(port_v, ",")
+		for _, p := range ps {
+			i, _ := strconv.Atoi(p)
+			ports = append(ports, uint16(i))
+		}
+	}
 
-  for _, v := range ports {
-    go connect(host+":"+strconv.Itoa(int(v)), ch)
-  }
+	for _, v := range ports {
+		go connect(host+":"+strconv.Itoa(int(v)), ch)
+	}
 
-  for i := 0; i < len(ports); i++ {
-    r := string(<-ch)
-    if !all && strings.Contains(r, "False") {
-      continue
-    }
-    println(r)
-  }
+	for i := 0; i < len(ports); i++ {
+		r := string(<-ch)
+		if !all && strings.Contains(r, "False") {
+			continue
+		}
+		println(r)
+	}
 }
